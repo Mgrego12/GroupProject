a84cae9f5ecb46abe031bd1c3069aab151436b7b
Matthew Gregorek <132983612+Mgrego12@users.noreply.github.com>
2023-08-08 22:09:39 -0400

    Update ChatBot.java

--- CoreNLP_JavaBot/src/ChatBot.java
+++ CoreNLP_JavaBot/src/ChatBot.java
@@ -1,12 +1,6 @@
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 import javax.swing.*;
 
 /*
@@ -16,12 +10,13 @@ import javax.swing.*;
  * 
  * Goals
  * 	- Make Chatbot-User interaction more Robust with more capable Answers
- * 	- Make the GUI more Appealing for the User. 
- *  - Clean up CoreNLP Analysis Area, add new JButton "CoreNLP" for User to press
- *  	to display the Real Time Analysis
+ * 	- Make the GUI more Appealing for the User. The Time/Date/Math functions should be called 
+ * 		from the Processes.java inside the GUI. 
+ *  - Clean up CoreNLP Analysis Area, add new JButton "Analysis" for User to press
+ *  	to display the Real Time Analysis / Hide Analysis if they Want to. 
  *  - Provide More Information about how CoreNLP works and Make the Definitions more accessible
  *  - If time permits, start providing more Secure Variable/Methods/Classes , add exception handling 
- *  	and try/catch methods where viable. 
+ *  	and try/catch methods where viable and J-Unit tests for major class methods. 
  *  - Provide more Functionality for the bot to answer (Date/Time/Math) 
  */
 
@@ -38,11 +33,19 @@ public class ChatBot extends JFrame {
 	private final Processes chatBotProcesses; // Add 'final' keyword here
 	private JButton startAnalysisButton;
 	private JSplitPane splitPane;
+	
 
 	public ChatBot() {
+		/*
+		 * GUI for ChatBot constructor, we need to initialize Processes and
+		 * CoreNLP classes , create ChatBot Area on the Left, Analysis Area on the Right
+		 * add Buttons to the bottom of the Layout next to the TextField which will take in
+		 * User input when "Send" button is Called. 
+		 */
 		this.chatBotProcesses = new Processes(); // initialize BotProcesses variable
 		analyzeBot = new CoreNLP(); // initialize CoreNLP
 		this.coreDefinitions = new CoreNLP.NLP_CORE_DEFINITIONS();
+
 
 		setLayout(new BorderLayout());
 
@@ -61,12 +64,14 @@ public class ChatBot extends JFrame {
 		analysisArea.setForeground(Color.BLACK);
 		analysisArea.setEditable(false);
 		JScrollPane analysisScrollPane = new JScrollPane(analysisArea);
-		
+
 		JPanel chatAnalysisContainer = new JPanel(new BorderLayout());
-		
+
 		splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT) {
 			/**
-			 * ensures that the splitPane will be recognized 
+			 * ensures that the splitPane will be recognized by using
+			 * invokeLater() method, very helpful when working with a lot of
+			 * GUI components at once. 
 			 */
 			private static final long serialVersionUID = 1L;
 			@Override
@@ -77,7 +82,12 @@ public class ChatBot extends JFrame {
 				});
 			}
 		};
-
+		/*
+		 * SplitPane to show chatBox on Left, Analysis on Right
+		 * initialize input TextField and SendButton/analysisButton
+		 * Create box and add textfields and buttons to the box
+		 */
+		
 		splitPane.setLeftComponent(chatScrollPane);
 		splitPane.setRightComponent(analysisScrollPane);
 		chatAnalysisContainer.add(splitPane, BorderLayout.CENTER);
@@ -90,14 +100,13 @@ public class ChatBot extends JFrame {
 		userInput.setBackground(Color.GRAY);
 		userInput.setForeground(Color.BLACK);
 		sendButton.setBackground(Color.BLACK);
-		sendButton.setForeground(Color.WHITE);;
+		sendButton.setForeground(Color.WHITE);
+		;
 		box.add(userInput);
 		box.add(sendButton);
 		box.add(startAnalysisButton);
 		add(chatAnalysisContainer, BorderLayout.CENTER);
 		add(box, BorderLayout.SOUTH);
-
-
 
 		userInput.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -107,6 +116,12 @@ public class ChatBot extends JFrame {
 
 		sendButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				/*
+				 * Send Button action event, will collect user input
+				 * and append input to chatArea (left) . Analyze using
+				 * processInput and analyzeText from the Processes Class.
+				 * append response to ChatArea, and Analysis to analysisArea
+				 */
 				String input = userInput.getText().trim();
 				if (!input.isEmpty()) {
 					chatArea.append("User: " + input + "\n");
@@ -115,13 +130,17 @@ public class ChatBot extends JFrame {
 					chatArea.append("Bot : " + response + " \n");
 					analysisArea.append(analyze);
 					userInput.setText("");
-					
+
 				}
 			}
 		});
 		// analysis Area for the GUI , should show CoreNLP annotation
 		startAnalysisButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				/*
+				 * analysisButton actionListener to display or Hide
+				 * CoreNLP analysis area (to the Right) 
+				 */
 				if (analysisArea.isVisible()) {
 					analysisArea.setVisible(false);
 					startAnalysisButton.setText("Show Analysis");
@@ -137,6 +156,11 @@ public class ChatBot extends JFrame {
 	}
 
 	public static void main(String[] args) {
+		/*
+		 * @MAIN method - Use invokeLater for Safety Precautions to make sure
+		 * all other functionality is called before chatbot, welcome message, 
+		 *  and GUI visibility are called. 
+		 */
 		SwingUtilities.invokeLater(() -> {
 			ChatBot chatbot = new ChatBot();
 
